f3cf0e17cc6522ea01d2793afbdca2e93a18c9e0
Anchen Ko <70150536+0ako@users.noreply.github.com>
2021-03-17 18:47:50 +0800

    TODO, a bunch, start with TODO 1

--- readme.md
+++ readme.md
@@ -2,16 +2,32 @@ Built with guidance from:
 https://www.freecodecamp.org/news/how-to-build-an-html-calculator-app-from-scratch-using-javascript-4454b8714b98/
 
 # Notes:
-Check !! comments on index.html for learning points
+Check comments on index.html for learning points
 Techniques to familiarize myself with:
+
+- Determining and building out the happy path first! (average user flow)
+- The console.log() method prints out a toString representation of the object in the console to the user.
+- The console.dir() method output the list of object properties of a specified object in the console to the user.
+    - console.dir(e.target); prints DOM elements in Chrome console! Provides actionable information on the target of event
 
 - CSS grid
 - Assigning and using data-action attributes
     - For all operator keys, clear, decimal and equals key 
 - DOM methods:
     - Listen to all keypresses and determining which key/type was pressed: 
-- Determining and building out the happy path first (average user flow)
-    - Happy path: user hits number, decimal, operator, number...
+- Happy path programming: 
+    - User hits number, decimal, operator, number...
         - When user hits number:
             - If calculator shows 0, target number replaces it (Find out current displayed number using .calculator__display)
             - If calculator shows non-zero number, append target number to displayed number (Find using textContent)
+- Add class to element with CSS changes to highlight button changes
+    - add is-depressed to key to operator key to highlight pressed
+- Remove class from all keys using forEach loop
+    - 
+- Use custom attributes to store input values!!
+    - add data-set-previous-key-type to know if operator was the last key pressed. 
+    - store the first number of a calculation in a custom attribute before we wipe it clean: calculator.dataset.firstValue = displayedNum
+    - likewise, store the operator last clicked
+- convert string to float number inline with parseFloat (parseInt also exists)
+- Grab text content of HTML element using element.textContent
+    - key.textContent grabs text content of HTML element assigned to key
--- script.js
+++ script.js
@@ -2,32 +2,66 @@ const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
 
 const display = calculator.querySelector('.calculator__display');
+const previousKeyType = calculator.dataset.previousKeyType; // grabs DOM data attribute assigned to calculator class
+
+const calculate = function (n1, operator, n2) {
+    let result = '';
+
+    if (operator == "add"){
+        result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator == "subtract"){
+        result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator == "multiply"){
+        result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator == "divide"){
+        result = parseFloat(n1) / parseFloat(n2);
+    }
+    return result;
+}
 
 keys.addEventListener('click', e => {
-    if(e.target.matches('button')) {
-        // console.log(e)
-        const key = e.target;
-        const action = key.dataset.action // DOM path: target.dataset.action 
-        let keyContent = key.textContent; // !! this grabs textContent from key variable
-        let displayedNum = display.textContent;
+    if(e.target.matches('button')) { // element.matches() also looks for matches in DOM paths 
+        console.dir(e.target)
+        const key = e.target; // set the clicked element (target) to a variable
+        const action = key.dataset.action // set the target's data-action attribute to a variable. Use it to determine which action button was clicked
+        let keyContent = key.textContent; // set the target's HTML text content to a variable!
+        let displayedNum = display.textContent; // set display's (element with '.calculator__display') HTML text content to a variable 
         
-        if(!action) { 
+        if(!action) { // therefore, is number button
             console.log('number key!')
-            if(displayedNum == '0') {
-                display.textContent = keyContent; // !! must assign value to element.textContent, otherwise textContent won't update on the page
+            if(display.textContent === '0'|| calculator.dataset.previousKeyType === 'operator') { // if either condition is true, sets display to key.textContent. TODO remove previousKeyType 'operator' after next number after operator is pressed, so that display updates properly after operator was pressed
+                display.textContent = key.textContent; // assign the value to display.textContent, not the equivalent JS variable, otherwise it won't live update 
             } else {
-                display.textContent += keyContent;
+                display.textContent = display.textContent + key.textContent;
             } 
         }
         if(action == "add" ||
            action == "subtract" ||
            action == "multiply" ||
            action == "divide"
-        ) console.log('operator key')
-        if(action == 'calculate') console.log("equals key")
-        if(action == 'decimal') console.log("decimal key")
-        if(action == 'clear') console.log("clear key")
-
+        ) {
+            console.log('operator key')
+            key.classList.add('is-depressed')
+            calculator.dataset.previousKeyType = 'operator'; // sets data-previous-key-type attribute for .calculator element, to determine if last key clicked was an operator
+            calculator.dataset.firstValue = display.textContent; // stores display.textContent to data-first-value attribute for .calculator element, for later use
+            calculator.dataset.operator = action; // stores event target data-action attribute to data-operator attribute for .calculator element, for later use
+        }
+        if(action == 'calculate') {
+            console.log("equals key")
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = display.textContent; 
+            calculate(firstValue, operator, secondValue); // TODO: display.textContent = result
+        }
+        if(action == 'decimal') {
+            console.log("decimal key")
+            display.textContent += key.textContent; // TODO: prevent multiple decimal points from being inserted
+        }
+        if(action == 'clear') {
+            console.log("clear key")
+            display.textContent = 0;
+        }
+        // Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed')) // TODO: remove after Mary hits number key again
     }
 })
 
