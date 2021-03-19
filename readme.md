766ac3da3cfc99ee2c946815a31386dc0d548da2
Anchen Ko <70150536+0ako@users.noreply.github.com>
2021-03-19 18:01:36 +0800

    todo: make sure data-first-value and display.textContent are updated with every click of calculate

--- readme.md
+++ readme.md
@@ -2,8 +2,14 @@ Built with guidance from:
 https://www.freecodecamp.org/news/how-to-build-an-html-calculator-app-from-scratch-using-javascript-4454b8714b98/
 
 # Notes:
+
+Biggest challenge when building a calculator: chaining operations together: 12 + 7 - 5 * 3 = should yield 42
+
 Check comments on index.html for learning points
 Techniques to familiarize myself with:
+
+- Understand all the rules of a calculator before writing code:
+    - The calculator should treat first and second values as the same numbers if it's missing a value. i.e. 2 + = 4
 
 - Determining and building out the happy path first! (average user flow)
 - The console.log() method prints out a toString representation of the object in the console to the user.
@@ -24,7 +30,7 @@ Techniques to familiarize myself with:
     - add is-depressed to key to operator key to highlight pressed
 - Remove class from all keys using forEach loop
     - 
-- Use custom attributes to store input values!!
+- Use custom attributes to store previously inputted values!!
     - add data-set-previous-key-type to know if operator was the last key pressed. 
     - store the first number of a calculation in a custom attribute before we wipe it clean: calculator.dataset.firstValue = displayedNum
     - likewise, store the operator last clicked
@@ -34,3 +40,12 @@ Techniques to familiarize myself with:
 - Use forEach with Array.from() to cycle through every element in a nodeList
     - To remove a class from all the children nodes of a parentNode
     - Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
+- To prevent something from being inputted more than once, use includes()
+    - if(!display.textContent.includes('.'))
+- Just add new rules/conditions if you need them!
+    - If calculate should only run when operator has been clicked, i.e. firstValue exists, just use if(firstValue)
+    - If the display.textContent needs to show "0." when previousKeyType is "operator" just add the conditional statement!
+        - if(calculator.dataset.previousKeyType == "operator") {display.textContent = "0."}
+- Just append new conditions to existing ones!
+- WRITE IT OUT! To understand chains of functions (flowcharts) Write down what is happening with our function AND what is SUPPOSED to happen with our function.
+    - To understand our calculate function, and which values need to be assigned to which variables to chain calculations together, get it down on paperrrr
--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const calculate = function (n1, operator, n2) {
 
 keys.addEventListener('click', e => {
     if(e.target.matches('button')) { // element.matches() also looks for matches in DOM paths 
-        console.dir(e.target)
+        // console.dir(e.target)
         const key = e.target; // set the clicked element (target) to a variable
         const action = key.dataset.action // set the target's data-action attribute to a variable. Use it to determine which action button was clicked
         let keyContent = key.textContent; // set the target's HTML text content to a variable!
@@ -29,11 +29,10 @@ keys.addEventListener('click', e => {
         
         Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed')) // Cycles through all children nodes of parentNode (i.e. all keys) to remove class. It's run before class is added
 
-        if(!action) { // therefore, is number button
-            console.log('number key!')
-            if(display.textContent === '0'|| calculator.dataset.previousKeyType === 'operator') { // if either condition is true, sets display to key.textContent
-                display.textContent = key.textContent; // assign the value to display.textContent, not the equivalent JS variable, otherwise it won't live update 
-                calculator.removeAttribute('data-previous-key-type'); // !! not mentioned in article
+        if(!action) { // therefore, is number button 
+            if(display.textContent === '0'|| calculator.dataset.previousKeyType === 'operator' || calculator.dataset.previousKeyType === 'calculate') { // if either condition is true, sets display to key.textContent
+                display.textContent = key.textContent; // (!) assign key value to display.textContent, not the equivalent JS variable, otherwise it won't live update 
+                calculator.dataset.previousKeyType = "number";
             } else {
                 display.textContent = display.textContent + key.textContent;
             } 
@@ -43,29 +42,48 @@ keys.addEventListener('click', e => {
            action == "multiply" ||
            action == "divide"
         ) {
-            console.log('operator key')
-            key.classList.add('is-depressed')
-            calculator.dataset.previousKeyType = 'operator'; // sets data-previous-key-type attribute for .calculator element, to determine if last key clicked was an operator
-            calculator.dataset.firstValue = display.textContent; // stores display.textContent to data-first-value attribute for .calculator element, for later use
-            calculator.dataset.operator = action; // stores event target data-action attribute to data-operator attribute for .calculator element, for later use
-        }
-        if(action == 'calculate') {
-            console.log("equals key")
             const firstValue = calculator.dataset.firstValue;
             const operator = calculator.dataset.operator;
             const secondValue = display.textContent; 
-            display.textContent = calculate(firstValue, operator, secondValue); 
-            calculator.dataset.previousKeyType = 'operator'
+            if(firstValue && operator && calculator.dataset.previousKeyType !== 'operator') { // should calculate when firstValue and operator exist (secondValue always exists, so no need to check it), and if previousKeyType is not 'operator'
+                display.textContent = calculate(firstValue, operator, secondValue);
+                calculator.dataset.firstValue = display.textContent
+            }
+            key.classList.add('is-depressed')
+            calculator.dataset.firstValue = display.textContent; // stores display.textContent to data-first-value attribute for .calculator element, for later use
+            calculator.dataset.operator = action; // stores event target data-action attribute to data-operator attribute for .calculator element, for later use
+            calculator.dataset.previousKeyType = 'operator'; // sets data-previous-key-type attribute for .calculator element, to determine if last key clicked was an operator
         }
-        if(action == 'decimal') {
-            if(!display.textContent.includes('.')){
-                console.log("decimal key")
+        if(action == 'calculate') { // todo review this block
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            let secondValue = display.textContent;  // ?? not supposed to be const, as in guide
+             
+            if(firstValue) { // if operator has been hit 
+                
+                if(calculator.dataset.previousKeyType == 'calculate') {  // problem: if we hit calculate again, then it should -1 -1 -1 -1 every time
+                    calculator.dataset.firstValue = display.textContent; // firstValue and display alternately change only every other click
+                    secondValue = calculator.dataset.modValue; 
+                }
+                display.textContent = calculate(firstValue, operator, secondValue);  // !! only runs every other click. SHOULD set every click
+            }
+            calculator.dataset.modValue = secondValue;
+            calculator.dataset.previousKeyType = 'calculate';
+        }
+
+        if(action == 'decimal') { 
+            if (calculator.dataset.previousKeyType == 'operator' || calculator.dataset.previousKeyType == "calculate") {
+                display.textContent = "0.";
+            } else if (!display.textContent.includes('.')){
                 display.textContent += key.textContent; 
             }
+            calculator.dataset.previousKeyType = 'decimal';       
         }
-        if(action == 'clear') {
-            console.log("clear key")
-            display.textContent = 0;
+        if(action == 'clear') { 
+            display.textContent = 0; //todo should make display 0, not disappear
+            calculator.removeAttribute('data-first-value');
+            calculator.removeAttribute('data-operator');
+            calculator.dataset.previousKeyType = 'clear';
         }
     }
 })
